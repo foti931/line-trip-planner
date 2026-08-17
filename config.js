df37cfeca79a4e3296e2a5573914ce5bd24b24bc
takuya.s <takublo.gk@gmail.com>
2026-08-17 22:50:27 +0900

    Configure Supabase shared storage

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LINE_TRIP_PLANNER_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://zqehtaokjjqjcxkiwtup.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_emZY1J9eTMwlshQunfjgbw_dWV7BZGe",
   DEFAULT_TRIP_ID: "main-trip"
 };
